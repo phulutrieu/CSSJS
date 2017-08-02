d149e6a54a25fab17d25c3cf19c66a0129e9a6df
phulutrieu <30656709+phulutrieu@users.noreply.github.com>
2017-08-02 20:13:28 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://goo.gl/Z8DtA1'>https://goo.gl/Z8DtA1"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://maukesachtreotuong.blogspot.com'>https://maukesachtreotuong.blogspot.com"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
